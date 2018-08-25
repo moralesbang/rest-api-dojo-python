ea1b84c78f51b265b6cac276f781c0c58d9b2f34
Juan Camilo Morales <camov1209@gmail.com>
2018-08-25 11:55:58 -0500

    Creates rest api

---
+++ dojoproject/dojoapp/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Estudiante, Curso
+
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+
+# Register your models here.
